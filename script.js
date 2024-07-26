8e64493bb27d627daa7437fcf700de994de430f6
eyeball420 <157904828+eyeball420@users.noreply.github.com>
2024-07-26 16:19:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let slideIndex = 0;
+let slideIndex1 = 0;
 showSlides();
+showSlides1();
 
 function showSlides() {
   let i;
@@ -11,4 +13,16 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block";  
   setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+function showSlides1() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides2");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex1++;
+  if (slideIndex1 > slides.length) {slideIndex1 = 1}    
+  slides[slideIndex1-1].style.display = "block";  
+  setTimeout(showSlides1, 3000); // Change image every 3 seconds
 }
